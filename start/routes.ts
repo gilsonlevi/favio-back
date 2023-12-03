b89e4c6d91ee69d78a4f96bc74b98a48d593c009
gilsonlevi <gilsonlevi328@gmail.com>
2023-12-02 22:33:43 -0300

    Melhorando metodos http

--- start/routes.ts
+++ start/routes.ts
@@ -1,50 +1,33 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
+// ROTAS
 import Route from '@ioc:Adonis/Core/Route'
 
+// Array de favoritos
 const favoritos = [
-  { id: 1, nome: 'IFRN', url: 'http://www.ifrn.com', importante: true },
-  { id: 2, nome: 'google', url: 'http://www.google.com', importante: true },
-  { id: 3, nome: 'uol', url: 'http://www.uol.com', importante: true },
+  { id: 1, nome: 'IFRN', url: 'http://www.ifrn.com.br', importante: true },
+  { id: 2, nome: 'GOOGLE', url: 'http://www.google.com.br', importante: true },
+  { id: 3, nome: 'UOL', url: 'http://www.uol.com.br', importante: true },
 ]
 
+// Rota padrão
 Route.get('/', async () => {
   return { hello: 'world' }
 })
 
+// Rota para pegar todos os favoritos
 Route.get('/favoritos', async () => {
   return favoritos
 })
 
 // Metodo GET para buscar favoritos pelo ID
 Route.get('/favoritos/:id', async ({ params, response }) => {
-  // console.log(array)
-  // array.map((e)=>console.log(e.id))
   const found = favoritos.find((favorito) => favorito.id == params.id)
   if (found == undefined) {
-    return response.status(404)
+    return response.status(404).send({})
   }
   return found
 })
 
+// Metodo GET para buscar favoritos pelo nome
 Route.get('/favoritos/:nome', async ({ params }) => {
   return { id: 1, nome: params.nome, url: 'http://www.google.com', importante: true }
 })
@@ -53,11 +36,13 @@ Route.get('/favoritos/:nome', async ({ params }) => {
 Route.post('/favoritos', async ({ request, response }) => {
   const { nome, url, importante } = request.body()
   const newFavorito = { id: favoritos.length + 1, nome, url, importante }
-  favoritos.push(newFavorito)
+  
   if (newFavorito.nome == null || newFavorito.nome == null || newFavorito.importante == null) {
     return response.status(400).send(newFavorito)
+  }else{
+    favoritos.push(newFavorito)
+    return response.status(201).send(newFavorito)
   }
-  return response.status(201).send(newFavorito)
 })
 
 Route.delete('/favoritos', async ({ request, response }) => {
