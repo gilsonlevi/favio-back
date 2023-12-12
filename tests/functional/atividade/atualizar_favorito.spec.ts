ba512ac16fdc33f7a3b6d51a8c92c5bda79ac6c8
gilsonlevi <gilsonlevi328@gmail.com>
2023-12-12 19:52:22 -0300

    Descomentando atualizar favoritos

--- tests/functional/atividade/atualizar_favorito.spec.ts
+++ tests/functional/atividade/atualizar_favorito.spec.ts
@@ -1,19 +1,19 @@
-// import { test } from '@japa/runner'
+import { test } from '@japa/runner'
 
-// test.group('Atualizar favorito', () => {
-//   test('atualizar favorito', async ({client}) => {
-//     const respota = await client.put('/favoritos/6').json({nome: 'SGA', url: 'sga.com.br', importante: true})
-//     respota.assertStatus(201)
-//   })
+test.group('Atualizar favorito', () => {
+  test('atualizar favorito', async ({client}) => {
+    const respota = await client.put('/favoritos/6').json({nome: 'SGA', url: 'sga.com.br', importante: true})
+    respota.assertStatus(201)
+  })
 
-//   test('atualizando favorito inexistente', async ({client}) => {
-//     const resposta = await client.put('favoritos/idnaoexiste').json({nome: 'SGA', url: 'sga.com.br', importante: true})
-//     resposta.assertStatus(404)
-//   })
+  test('atualizando favorito inexistente', async ({client}) => {
+    const resposta = await client.put('favoritos/idnaoexiste').json({nome: 'SGA', url: 'sga.com.br', importante: true})
+    resposta.assertStatus(404)
+  })
 
-//   test('atualizando favorito com campo faltante', async ({client}) => {
-//     const resposta = await client.put('favoritos/3').json({nome: 'SGA', importante: true})
-//     resposta.assertStatus(201)
-//     resposta.assertBodyContains({id: 3, nome: 'SGA', url: 'http://www.uol.com.br', importante: true })
-//   })
-// })
+  test('atualizando favorito com campo faltante', async ({client}) => {
+    const resposta = await client.put('favoritos/3').json({nome: 'SGA', importante: true})
+    resposta.assertStatus(201)
+    resposta.assertBodyContains({id: 3, nome: 'SGA', url: 'http://www.uol.com.br', importante: true })
+  })
+})
