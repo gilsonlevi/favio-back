77ccb1e23ac1855b10198e8f329877721f83045f
Gilson Levi <117370090+gilsonlevi@users.noreply.github.com>
2023-12-15 09:28:42 -0300

    Iniciando com sqlite

--- app/Controllers/Http/FavoritosController.ts
+++ app/Controllers/Http/FavoritosController.ts
@@ -1,17 +1,37 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+const favoritos = [{ id: 1, nome: 'IFRN', url: 'http://www.google.com.br', importante: true }]
+
 export default class FavoritosController {
-  public async index({}: HttpContextContract) {}
+  public async index({}: HttpContextContract) {
+    return favoritos
+  }
 
   public async create({}: HttpContextContract) {}
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, response }: HttpContextContract) {
+    const { nome, url, importante } = request.body()
+    const newFavorito = { id: favoritos.length + 1, nome, url, importante }
 
-  public async show({}: HttpContextContract) {
-    return 'show'
+    const found = favoritos.find((favorito) => favorito.nome == nome && favorito.url == url)
+    if (newFavorito.nome == null || newFavorito.url == null) {
+      return response.status(400).send(newFavorito)
+    } else if (found !== undefined) {
+      return response.status(400)
+    } else {
+      favoritos.push(newFavorito)
+      return response.status(201).send(newFavorito)
+    }
   }
 
-  public async edit({}: HttpContextContract) {}
+  public async show({ params, response }: HttpContextContract) {
+    const found = favoritos.find((favorito) => favorito.id == params.id)
+    if (found == undefined) {
+      return response.status(404).send({})
+    } else {
+      return response.status(200).send(found)
+    }
+  }
 
   public async update({}: HttpContextContract) {}
 
